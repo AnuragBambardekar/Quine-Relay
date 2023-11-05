13cf4cb9839a18f02b959cd7d33dc3e9c5563b9e
AnuragBambardekar <anurag.bambardekar@gmail.com>
2023-11-05 11:14:02 -0500

    installed F# and made my first script in F#

---
+++ FirstIonideProject/Program.fs
@@ -0,0 +1,14 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+// printfn "Hello from F#"
+
+let toPigLatin (word: string) =
+    let isVowel (c: char) =
+        match c with
+        | 'a' | 'e' | 'i' |'o' |'u'
+        | 'A' | 'E' | 'I' | 'O' | 'U' -> true
+        |_ -> false
+    
+    if isVowel word[0] then
+        word + "yay"
+    else
+        word[1..] + string(word[0]) + "ay"
