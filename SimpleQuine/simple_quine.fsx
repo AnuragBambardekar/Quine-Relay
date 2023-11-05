6a29f9ec24b08fa76c6b24a4a2029400e004772b
AnuragBambardekar <anurag.bambardekar@gmail.com>
2023-11-05 11:14:27 -0500

    made a simple Quine in F#

---
+++ SimpleQuine/simple_quine.fsx
@@ -0,0 +1,24 @@
+﻿let data = [
+ "let data = [";
+ " ]";
+ "let q = string(char 34)";
+ "let semicolon = string(char 59)";
+ "let space = string(char 32)";
+ "let boundary = 1";
+ "for d in data[..boundary-1] do";
+ "   System.Console.WriteLine(d)";
+ "for d in data do";
+ "   System.Console.WriteLine(space + q + d + q + semicolon)";
+ "for d in data[boundary..] do";
+ "   System.Console.WriteLine(d)";
+ ]
+let q = string(char 34)
+let semicolon = string(char 59)
+let space = string(char 32)
+let boundary = 1
+for d in data[..boundary-1] do
+   System.Console.WriteLine(d)
+for d in data do
+   System.Console.WriteLine(space + q + d + q + semicolon)
+for d in data[boundary..] do
+   System.Console.WriteLine(d)
